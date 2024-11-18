2a8dbbc44379707769969fadf1950e8a2cd97c2e
Erik <eriky2005@mail.ru>
2024-11-18 22:40:19 +0300

    Попытка 3

--- scripts/index.js
+++ scripts/index.js
@@ -10,14 +10,14 @@
 const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
 
-function render() {
+function renderInitialCards() {
     initialCards.forEach(({ name, link }) => {
-        const card = addCard({ name, link }, deleteCard);
+        const card = createCard({ name, link }, deleteCard);
         cardsContainer.append(card);
     });
 }
 
-function addCard({ name, link }, deleteCard) {
+function createCard({ name, link }, deleteCard) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
@@ -34,4 +34,4 @@ function deleteCard(cardElement) {
     cardElement.remove();
 }
 
-render();
+renderInitialCards();
