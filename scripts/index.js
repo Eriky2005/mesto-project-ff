8fd7c29c3e405634426a9d032d16ca8730792da0
Erik <eriky2005@mail.ru>
2024-11-18 20:06:52 +0300

    Попытка 1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
+
+function render() {
+    initialCards.forEach(({ name, link }) => {
+        const card = addCard({ name, link }, deleteCard);
+        cardsContainer.append(card);
+    });
+}
+
+function addCard({ name, link }, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').alt = name;
+    
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+render();
